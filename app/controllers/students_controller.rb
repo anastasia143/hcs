ca4e58e723a329f7f0c9a58e590b81d77d24f488
anastasia143 <semenova.anastasia143@yandex.ru>
2014-04-07 00:25:18 +0400

    Edit and delete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,6 +17,21 @@ class StudentsController < ApplicationController
    def edit
       @student = Student.find(params[:id])
    end
+   def destroy
+       @student = Student.find(params[:id])
+       @student.destroy
+
+       redirect_to students_path
+   end
+   def update
+       @student = Student.find(params[:id])
+ 
+       if @student.update(student_params)
+           redirect_to @student
+       else
+           render 'edit'
+       end
+   end
    private
       def student_params
           params.require(:student).permit(:surname, :name, :phone, :h1t1, :h1t2, :h1t3, :h2t1, :h3t1, :h3t2)
